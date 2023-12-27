0f9666fba4525b11e131310ae2b9843d54b0861c
曾佳俊 <952432141@qq.com>
2023-12-27 16:54:32 +0800

    chore: 添加 ant-design 依赖

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import Antd from "ant-design-vue";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -8,7 +10,11 @@ import "animate.css";
 
 import "virtual:uno.css";
 
+import "ant-design-vue/dist/reset.css";
+
 const app = createApp(App);
+
+app.use(Antd);
 
 app.use(createPinia());
 app.use(router);
