c2dde33ec3de586758f0a9e34e9f3573fa0de17e
曾佳俊 <952432141@qq.com>
2023-12-28 11:22:54 +0800

    feat: 欢迎语修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,25 @@
 </template>
 
 <script setup lang="ts">
+import { h } from "vue";
+
 import { Modal } from "ant-design-vue";
 
 import LuckyBoard from "./lucky-board/index.vue";
 
+import FlowerIcon from "./flower-icon.vue";
+
+const content = [
+  h("p"),
+  h("p", "幸运翻翻乐 💐 ~ "),
+  h("p", "一定能带给你幸运的幸运幸运幸运板 🎉🎉 ~~ "),
+  h("p", "规则就是没有任何理由的幸运值爆满 🚀🚀🚀 ~~~ "),
+];
+
 Modal.info({
-  title: "欢迎使用",
-  content: "请开始您的翻牌之旅 ~ 规则就是没有规则",
+  title: "",
+  icon: h(FlowerIcon),
+  content: h("div", content),
   okText: "确定",
   closable: false,
   centered: true,
