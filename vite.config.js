01a1d7ace4e076f80e1716951e19446cee0dad21
曾佳俊 <952432141@qq.com>
2023-12-28 13:32:19 +0800

    feat: vite.config.js, 添加 base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/lucky-board",
   plugins: [vue(), vueJsx(), UnoCSS()],
   resolve: {
     alias: {
